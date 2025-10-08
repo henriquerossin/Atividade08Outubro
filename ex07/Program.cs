acf5a179ab31203cddc98e49a92fcd2f8b8dfbc8
Henrique <pro.henriquerossin@gmail.com>
2025-10-08 14:28:10 -0300

    fix ex 07

--- ex07/Program.cs
+++ ex07/Program.cs
@@ -1,39 +1,20 @@
-﻿/*
-Exercício 07:
-Sequência de Fibonacci
-- Solicite um número N para quantidade de termos.
-- Gere a sequência de Fibonacci com while ou for.
-- Use if para impedir que N seja menor que 1.
-*/
+﻿int Termos, Numero1 = 0, Numero2 = 0, NumeroAtual = 1;
 
-Console.WriteLine("Digite um número para saber a quantidade dos termos da sequência de Fibonacci");
-int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Digite a quantidade de termos da sequência de Fibonacci: ");
 
-int numAnterior = 0;
+Termos = int.Parse(Console.ReadLine());
 
-if (n >= 1)
+if (Termos > 1)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < Termos; i++)
     {
-        int num = i;
-        for (int j = 0; j < i; j++)
-        {
-            if (j == 0)
-            {
-                num = 1;
-            }
-            else
-            {
-                int temp = num;
-                num += numAnterior;
-                numAnterior = temp;
-            }
-        }
-        Console.WriteLine(num);
-        numAnterior = 0;
+        Console.WriteLine(NumeroAtual);
+        Numero1 = Numero2;
+        Numero2 = NumeroAtual;
+        NumeroAtual = Numero1 + Numero2;
     }
 }
 else
 {
-    Console.WriteLine("Número inválido. Digite um número maior ou igual a 1.");
+    Console.WriteLine("Digite um valor maior ou igual à 1: ");
 }
