f85a934d07200531067b0ebbbb7465b48afbfe71
Henrique <pro.henriquerossin@gmail.com>
2025-10-08 13:21:46 -0300

    Add project files.

---
+++ Atividade08Outubro/Program.cs
@@ -0,0 +1,13 @@
+﻿/* 
+Exercício 01:
+Tabuada com verificação de entrada
+- Solicite ao usuário um número entre 1 e 10.
+- Se o número estiver fora do intervalo, peça novamente (use while).
+- Imprima a tabuada desse número usando for.
+*/
+
+Console.WriteLine("Digite um número entre 1 e 10 para fazer a tabuada");
+
+
+
+
