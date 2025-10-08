0cdb6448b240eb86488cb265541feda6dd4fd435
Henrique <pro.henriquerossin@gmail.com>
2025-10-08 14:08:13 -0300

    feat: add ex 06

---
+++ ex06/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Exercício 06:
+Contar números pares e ímpares
+- Solicite 10 números ao usuário (usando for).
+- Para cada número, verifique se é par ou ímpar.
+- Ao final, exiba a quantidade de pares e ímpares digitados.
+*/
+
+Console.WriteLine("Digite 10 números: ");
+int countPares = 0;
+int countImpares = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Número {i + 1}: ");
+    int numero = int.Parse(Console.ReadLine()!);
+    if (numero % 2 == 0)
+    {
+        countPares++;
+    }
+    else
+    {
+        countImpares++;
+    }
+}
+
+Console.WriteLine($"Quantidade de números pares: {countPares}");
+Console.WriteLine($"Quantidade de números ímpares: {countImpares}");
