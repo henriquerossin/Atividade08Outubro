2b4bff0aa6466e46bbd7e0bde29ece42a5bff52d
Henrique <pro.henriquerossin@gmail.com>
2025-10-08 13:42:00 -0300

    feat: adiciona ex 03

---
+++ ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Exercício 03:
+Contagem regressiva
+- Solicite um número inteiro positivo.
+- Se for negativo ou zero, peça novamente (com do...while).
+- Faça uma contagem regressiva até 0 usando for.
+*/
+
+int num;
+
+do
+{
+    Console.WriteLine("Digite um número inteiro, positivo e maior que 0: ");
+    num = int.Parse(Console.ReadLine()!);
+} while (num <= 0);
+
+for (int i = num -1; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
