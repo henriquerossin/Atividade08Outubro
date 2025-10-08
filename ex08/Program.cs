21a7158021f215932a351878aee85bd29dafc217
Henrique <pro.henriquerossin@gmail.com>
2025-10-08 14:37:45 -0300

    feat: add ex 08

---
+++ ex08/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Exercício 08:
+Jogo de adivinhação
+- Gere um número aleatório de 1 a 50.
+- Use do...while para permitir tentativas até o usuário acertar.
+- Informe se o número digitado é maior ou menor que o número secreto.
+*/
+
+Random random = new Random();
+
+int NumeroSecreto = random.Next(1, 51);
+
+Console.WriteLine("Tente adivinhar o número secreto!");
+Console.WriteLine("Digite um número entre 1 e 50: ");
+
+int Tentativa = int.Parse(Console.ReadLine()!);
+
+while (Tentativa != NumeroSecreto)
+{
+    if (Tentativa < NumeroSecreto)
+    {
+        Console.WriteLine("O número é maior! Tente novamente: ");
+    }
+    else
+    {
+        Console.WriteLine("O número é menor! Tente novamente: ");
+    }
+    Tentativa = int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine($"Parabéns! Você acertou o número secreto! O número era {NumeroSecreto}");
