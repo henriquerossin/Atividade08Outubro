6c729abee00bc0f630b21b51544b205a95fa2fb0
Henrique <pro.henriquerossin@gmail.com>
2025-10-08 17:01:31 -0300

    feat: adiciona ex 09

---
+++ ex09Tarefa/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Peça um número N para o usuário. O número será fatorado. 
+*/
+
+Console.Write("Digite um número inteiro positivo para ser fatorado: ");
+
+int N = int.Parse(Console.ReadLine()!);
+
+int fatorial = 1;
+
+for (int i = N; i > 1; i--)
+{
+    fatorial *= i;
+}
+
+Console.WriteLine($"O fatorial de {N} é: {fatorial}");
