c924e4c05631eb8504c87cc0a12c960360060153
Henrique <pro.henriquerossin@gmail.com>
2025-10-08 13:35:52 -0300

    feat: adiciona ex 02

---
+++ ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Exercício 02:
+Somatório de números positivos
+- Permita que o usuário digite números até que insira um número negativo.
+- Some somente os números positivos informados.
+- Ao final, exiba o total.
+*/
+
+int num;
+int soma = 0; 
+
+do
+{
+    Console.WriteLine("Digite um número");
+    num = int.Parse(Console.ReadLine());
+    soma += num;
+} while (num >= 0);
+
+Console.WriteLine($"A soma dos números positivos é {soma}");
